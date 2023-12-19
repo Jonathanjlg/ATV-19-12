44f2a8466d406624ff04ff0382017aa767322b86
IgorWinMac <38919725+IgorWinMac@users.noreply.github.com>
2023-12-19 20:02:07 -0300

    ATV-19-12

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/*Console.WriteLine("Digite um número: ");
+var numero = int.Parse(Console.ReadLine());
+
+var sucessor = numero + 1;
+var antecessor = numero - 1;
+
+Console.WriteLine("O sucessor de {0} é {1}", numero, sucessor);
+Console.WriteLine("E o antecessor é {1}", numero, antecessor); */
+
+Console.WriteLine("Digite um número");
+int numero = int.Parse(Console.ReadLine());
+Console.WriteLine("Sucessor: {0}", numero+1);
+
+
+Console.WriteLine("Antecessor: {0}", numero-1);
